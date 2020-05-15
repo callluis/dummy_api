a7c518c7fc9e629a5b5286daa1dbd1ebecc775f4
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2020-05-15 12:53:09 -0400

    Modified jwt expiration

--- conf/config.py
+++ conf/config.py
@@ -13,7 +13,7 @@ APP_NAME = 'Dummy API'
 # =============================================================================
 API_SECRET_KEY = os.environ.get('API_SECRET_KEY', 'whatever')
 SUPER_ADMIN_KEY = os.environ.get('SUPER_ADMIN_KEY', 'SUPER')
-JWT_EXPIRATION_SECONDS = int(os.environ.get('JWT_EXPIRATION_TIME', '30'))
+JWT_EXPIRATION_SECONDS = int(os.environ.get('JWT_EXPIRATION_TIME', '300'))
 
 # =============================================================================
 # Logging
--- middleware/jwt_authentication.py
+++ middleware/jwt_authentication.py
@@ -13,9 +13,12 @@ def validate_token(req, resp, resource, params):
         }
         raise generic_error_handler(401, error_override=error)
 
-    if not token_is_valid(req, token):
+    try:
+        token_is_valid(req, token)
+    except Exception as ex: 
         error = {
-            "description": "invalid token, try again"
+            "description": "invalid token, try again",
+            "details": ex
         }
         raise generic_error_handler(401, error_override=error)
 
@@ -24,18 +27,15 @@ def token_is_valid(req, token):
         token_type = "JWT"
     else:
         LOG.error("Token validation failed")
-        return False
+        raise Exception("Invalid token type -> Only 'JWT' allowed")
     
     req.jwt_token = token
     token = token.split(f"{token_type} ")[1]
 
     try:
-        options = {'verify_exp': False}
-
-        jwt_body = jwt.decode(token, API_SECRET_KEY, verify=True, algorithms=['HS256'], options=options)
+        jwt_body = jwt.decode(token, API_SECRET_KEY, verify=True, algorithms=['HS256'])
         if 'username' in jwt_body:
             req.username = jwt_body['username']
-        return True
     except Exception as ex:
         LOG.error(f"There was an error : {ex}")
-        return False
+        raise Exception(ex)
